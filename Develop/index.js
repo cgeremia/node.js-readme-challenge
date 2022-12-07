ec395c19d170728142a195096262cacb671a9883
Cristina Geremia <112133093+cgeremia@users.noreply.github.com>
2022-12-06 22:12:21 -0500

    update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -78,7 +78,7 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile("./dist/README.md", generateMarkdown(data), function (err) {
+  fs.writeFile("./README.md", generateMarkdown(data), function (err) {
     if (err) {
       return console.log(err);
     }
